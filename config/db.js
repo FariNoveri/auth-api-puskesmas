b2ea7ba4ede667c8306913467adad0417f5bedf7
Fari Noveri <cahayalunamaharani1@gmail.com>
2025-04-23 11:57:49 +0700

    Update user routes, API documentation, and browser setup for local testing

--- config/db.js
+++ config/db.js
@@ -3,14 +3,14 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: 'localhost',      // ganti kalau database host beda
   user: 'postgres',       // default user PostgreSQL
-  password: '220903', // ganti dengan passwordmu
+  password: '123', // ganti dengan passwordmu
   database: 'puskesmas',
   port: 5432              // default port PostgreSQL
 });
 
 // Tes koneksi
 pool.connect()
-  .then(() => console.log('Connected to PostgreSQL Database (rs2025).'))
+  .then(() => console.log('Connected to PostgreSQL Database (Puskesmas).'))
   .catch((err) => console.error('Connection error', err.stack));
 
 module.exports = pool;
