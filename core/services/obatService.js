98d1b00f54d1992d4cd7898ce7738fc3dcd58aee
Vionando <vionando_wira_mada@teknokrat.ac.id>
2025-04-23 15:03:11 +0700

    Create fungsi obat

--- core/services/obatService.js
+++ core/services/obatService.js
@@ -1,21 +1,31 @@
-const obatModel = require('../models/obatModel');
+const db = require('../../config/db');  // Importing database connection
 
+// Function to get all obat (medicines) with jenis obat (tablet, kapsul, etc.)
 const getAllObats = async () => {
-  try {
-    const obats = await obatModel.getAllObats();
-    return obats;
-  } catch (error) {
-    throw new Error('Error fetching obats');
-  }
+  const query = `
+    SELECT o.*, o.jenis_obat  -- Fetching the jenis_obat directly from the obat table
+    FROM m_obat o;
+  `;
+  const result = await db.query(query);  // Execute the query
+  return result.rows;  // Return the rows from the query
 };
 
-const createObat = async (nama_obat, satuan_id, gudang_id, stok, jenis_obat, tanggal_kadaluarsa, bpom, gambar_obat, keterangan) => {
-  try {
-    const newObat = await obatModel.createObat(nama_obat, satuan_id, gudang_id, stok, jenis_obat, tanggal_kadaluarsa, bpom, gambar_obat, keterangan);
-    return newObat;
-  } catch (error) {
-    throw new Error('Error creating obat');
-  }
+// Function to create a new obat (medicine)
+const createObat = async ({ nama_obat, jenis_obat, gudang_id, stok, tanggal_kadaluarsa, bpom, gambar_obat, keterangan }) => {
+  const query = `
+    INSERT INTO m_obat 
+      (nama_obat, jenis_obat, gudang_id, stok, tanggal_kadaluarsa, bpom, gambar_obat, keterangan)
+    VALUES 
+      ($1, $2, $3, $4, $5, $6, $7, $8)
+    RETURNING *;
+  `;
+  
+  const result = await db.query(query, [
+    nama_obat, jenis_obat, gudang_id, stok, tanggal_kadaluarsa, 
+    bpom, gambar_obat, keterangan
+  ]);
+  
+  return result.rows[0];  // Return the newly created obat (medicine)
 };
 
 module.exports = { getAllObats, createObat };
