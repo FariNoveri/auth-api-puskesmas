ddeae5b1e24dbcd30f88e2bb8bf9e7008430d354
Fari Noveri <cahayalunamaharani1@gmail.com>
2025-04-29 11:31:25 +0700

    Deskripsi perubahan pada server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger'); // Import swaggerSpec dari swagger.js
 const userRoutes = require('./routes/userRoutes'); // Pastikan file routes/userRoutes.js diimpor
+const unitLayananRoutes = require("./routes/unitLayananRoutes"); // Mengimpor router untuk unit layanan
+const gudangRoutes = require('./routes/gudangRoutes'); // Router untuk gudang
+const obatRoutes = require('./routes/obatRoutes'); // Mengimpor router untuk obat
+const satuanObatRoutes = require('./routes/satuanObatRoutes'); // Router satuan obat
+require('dotenv').config();
+
 
 // Middleware untuk JSON
 app.use(express.json());
@@ -12,6 +18,14 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Gunakan route untuk users
 app.use('/users', userRoutes);  // Pastikan /users di-routing dengan userRoutes
+app.use("/api/unit-layanan", unitLayananRoutes); // Menggunakan unitLayananRoutes untuk API /api/unit-layanan
+app.use("/api/gudang", gudangRoutes); // Menggunakan gudangRoutes untuk API /api/gudang
+app.use('/api/obat', obatRoutes); // Menggunakan obatRoutes untuk API /api/obat
+app.use('/api/satuan_obat', satuanObatRoutes); // Menggunakan satuanObatRoutes untuk API /api/satuan_obat
+
+
+// Routes
+app.use('/users', userRoutes); // API users
 
 // Routes lain
 app.get('/api/hello', (req, res) => {
