838abac54a8ac96fdebf11c325392995d716cb41
Fari Noveri <cahayalunamaharani1@gmail.com>
2025-04-29 11:51:30 +0700

    fix login

--- controllers/userController.js
+++ controllers/userController.js
@@ -197,6 +197,24 @@ const profileController = async (req, res) => {
   }
 };
 
+// Tambahkan ini di atas module.exports
+function generateAccessToken(user) {
+  return jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: '15m' }
+  );
+}
+
+function generateRefreshToken(user) {
+  return jwt.sign(
+    { id: user.id },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: '7d' }
+  );
+}
+
+
 module.exports = {
   getAllUsersController,
   createUserController,
